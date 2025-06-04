f4deb5ed34dc09c48892802a3fa7a27e45e4809d
tokaretin <tokaretin@gmail.com>
2025-06-04 17:34:06 +0200

    Создал одну папку и два проекта

--- Lessons_C#/Program.cs
+++ Lessons_C#/Program.cs
@@ -4,14 +4,17 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;*/
 
-namespace Lessons_C_
+namespace Lessons_C_ // пространство имен
 {
     internal class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World");
+            Console.WriteLine("Hello World");
             Console.WriteLine("Привет Илюха!!");
+            Console.WriteLine("Привет Илюха!!");
+            Console.WriteLine("Я победил эти коммиты!!");           
             Console.WriteLine("Я победил эти коммиты!!");           
         }
     }
---
+++ Сегодняшняя задачка/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Сегодняшняя_задачка
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Урааа");
+            //Console.ReadKey(); // Это нужно чтобы консоль не закрывалась
+        }
+    }
+}
