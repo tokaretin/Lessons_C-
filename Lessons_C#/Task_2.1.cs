c1abebb3bda93cb7b3e2d10c04d0434470dc61c7
tokaretin <tokaretin@gmail.com>
2025-06-04 14:07:27 +0200

    Пробую добавить в папку новый код

---
+++ Lessons_C#/Task_2.1.cs
@@ -0,0 +1,13 @@
+using System; // Пространствнные имена
+
+namespace Lessons_C_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Я победил эти коммиты!!");
+            Console.WriteLine("Я победил эти коммиты!!");
+        }
+    }
+}
